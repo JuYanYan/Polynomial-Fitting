b8679d504ca0aba7bee367754546d7a5236c0659
JuYan <33692395+JuYanYan@users.noreply.github.com>
2019-04-25 12:31:29 +0800

    Update ployfitting.h
    Modified some data types.It is not important for PC.However, for MCU such as MSP430G2 or 89C52, may you should use unsigned int  type(old version) rather than use int type.

--- ployfitting.h
+++ ployfitting.h
@@ -1,5 +1,5 @@
 #pragma once
 typedef double RealNumber;
-void             LoadData_Into_Matrix(RealNumber *m, const RealNumber *v, unsigned int anNum, unsigned int inNum);
-void             calcNormalEqCof(RealNumber *a, RealNumber *b, const RealNumber *y, unsigned int anNum, unsigned int inNum);
-void             DirectLU(RealNumber *a, RealNumber *x, unsigned int n);
+void             LoadData_Into_Matrix(RealNumber *m, const RealNumber *v, int anNum, int inNum);
+void             calcNormalEqCof(RealNumber *a, RealNumber *b, const RealNumber *y, int anNum, int inNum);
+void             DirectLU(RealNumber *a, RealNumber *x, int n);
