9d7f0f6ee9769fcb4dd1b7466394bb29b6a15c15
JuYan <33692395+JuYanYan@users.noreply.github.com>
2019-06-11 15:55:09 +0800

    Update and rename ployfitting.h to polyfitting.h

--- polyfitting.h
+++ polyfitting.h
@@ -1,5 +1,6 @@
 #pragma once
 typedef double RealNumber;
+#define BUFF2D(p, i, j, n)        ((p) + (i) * (n) + (j))
 void             LoadData_Into_Matrix(RealNumber *m, const RealNumber *v, int anNum, int inNum);
 void             calcNormalEqCof(RealNumber *a, RealNumber *b, const RealNumber *y, int anNum, int inNum);
 void             DirectLU(RealNumber *a, RealNumber *x, int n);
